21de3c4818aed9edcacef639bcc1d7e4986246ce
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-07-09 20:53:44 +0700

    add task form

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,25 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
 
   dummyTasks = DUMMY_TASKS;
+
+  get selectedUserTasks() {
+    return this.dummyTasks.filter((task) => task.userId === this.userId);
+  }
 
   onCompleteTask(taskId: string) {
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== taskId);
   }
 
-  get selectedUserTasks() {
-    return this.dummyTasks.filter((task) => task.userId === this.userId);
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
