97b30e74f024747748447cc46f05b5aa535aa206
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-07-09 10:22:49 +0700

    user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
