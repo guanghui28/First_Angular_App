eb36536ea674e9c59efa71c3540937ecd96d3cec
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-07-09 17:12:56 +0700

    complete task button

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { DUMMY_TASKS } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -12,31 +13,11 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
 
-  dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  dummyTasks = DUMMY_TASKS;
+
+  onCompleteTask(taskId: string) {
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== taskId);
+  }
 
   get selectedUserTasks() {
     return this.dummyTasks.filter((task) => task.userId === this.userId);
