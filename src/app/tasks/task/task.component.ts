b2487a2e14d1e9517c5470a2134f5b52bee91140
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-07-10 13:46:03 +0700

    convert to module

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,9 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe, UpperCasePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe, UpperCasePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
