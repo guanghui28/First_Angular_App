a806b31d78156b23e5135a56353dadf2079ef13f
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-07-10 11:04:21 +0700

    save data from local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -8,6 +8,13 @@ import { type NewTask, type Task } from './task/task.model';
 export class TasksService {
   private tasks: Task[] = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -20,9 +27,15 @@ export class TasksService {
       userId,
       id: Date.now().toString(),
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
